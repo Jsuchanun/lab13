98efb9fac7b6c611b834d6268bc9186ee302ec12
suchanun <suchaun_j@cmu.ac.th>
2024-12-27 15:55:07 +0700

    edit code

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,45 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double array[], int size, double results[]) {
+   
+    double sum = 0;
+    double product = 1;  
+    double reciprocalSum = 0; 
+    double maxVal = array[0];
+    double minVal = array[0];
+
+    for (int i = 0; i < size; i++) {
+        sum += array[i];
+        product *= array[i];
+        reciprocalSum += 1.0 / array[i];
+
+        if (array[i] > maxVal) maxVal = array[i];
+        if (array[i] < minVal) minVal = array[i];
+    }
+
+    
+    double arithmeticMean = sum / size;
+
+    
+    double variance = 0;
+    for (int i = 0; i < size; i++) {
+        variance += pow(array[i] - arithmeticMean, 2);
+    }
+    double standardDeviation = sqrt(variance / size);
+
+    
+    double geometricMean = pow(product, 1.0 / size);
+
+    
+    double harmonicMean = size / reciprocalSum;
+
+    
+    results[0] = arithmeticMean;
+    results[1] = standardDeviation;
+    results[2] = geometricMean;
+    results[3] = harmonicMean;
+    results[4] = maxVal;
+    results[5] = minVal;
+}
